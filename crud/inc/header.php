4a25c5c25aa4338a5e913b9efa91d4eeb615880c
Italo Matheus <76659133+newitalo@users.noreply.github.com>
2021-11-26 20:38:40 -0300

    Add files via upload

---
+++ crud/inc/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>CRUD com Bootstrap</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>css/bootstrap.min.css">
+    <link rel="" href="<?php echo BASEURL; ?>css/style.css">
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
+   
+    </style>
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+        <svg xmlns="http://www.w3.org/2000/svg" margin-inline= "500" width="60" height="60" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+</svg>
+          <a style="margin-inline: 10px;" href="<?php echo BASEURL; ?>index.php" class="navbar-brand" >Inicio</a>
+          
+
+        
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">          
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    Clientes <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="<?php echo BASEURL; ?>customers">Gerenciar Clientes</a></li>
+                    <li><a href="<?php echo BASEURL; ?>customers/add.php">Novo Cliente</a></li>
+                </ul>
+            </li>
+          </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <main class="container">
